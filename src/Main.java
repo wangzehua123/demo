72a8ee54218a3e30911e55148a1fff6709f26ae3
王泽华 <2295781233@qq.com>
2023-02-16 16:55:23 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        test();
+
+    }
+    public static void test(){
+        System.out.println("my name is wang");
+        ArrayList list = new ArrayList();
+    }
+}
